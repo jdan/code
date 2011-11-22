7472c04645329a17e656dfce2df6a6a0dede4f84
Jordan Scales <scalesjordan@gmail.com>
2011-11-22 14:14:40 -0500

    added a parameter to change max value

--- ProjectEuler/racket/e1.rkt
+++ ProjectEuler/racket/e1.rkt
@@ -1,1 +1,11 @@
 #lang racket
+
+(define (one-to-n n)
+  (/ (+ (* n n) n) 2))
+
+(define (euler1 max)
+  (- (+ (* 3 (one-to-n (floor (/ (- max 1) 3)))) 
+        (* 5 (one-to-n (floor (/ (- max 1) 5))))) 
+     (* 15 (one-to-n (floor (/ (- max 1) 15))))))
+
+(euler1 1000)
