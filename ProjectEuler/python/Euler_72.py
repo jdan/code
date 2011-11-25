07331b3ce160a382217a0cd610a614ad0aff6986
Jordan Scales <scalesjordan@gmail.com>
2011-11-25 16:20:41 -0500

    Euler 72 in python

--- ProjectEuler/python/Euler_72.py
+++ ProjectEuler/python/Euler_72.py
@@ -1,27 +1,15 @@
-def main():
-    total = 0
-    for i in range(2, 1000001):
-        total += totient(i)
-        
-    print 'Total: %s' % total
-    
-def totient(k):
-    n, d, t = 1, 1, k
-    curr = 2
-    while k > 1:
-        if k % curr == 0:
-            k /= curr
-            while k % curr == 0:
-                k /= curr
+#!/usr/bin/python
+
+def euler72(limit):
+    sieve = range(int(limit) + 1)
+    for i in range(2, len(sieve)):
+        if sieve[i] != i:
+            continue
+        for j in range(i, len(sieve), i):
+            sieve[j] *= i - 1
+            sieve[j] /= i
             
-            n *= curr - 1
-            d *= curr
-            
-            curr = 2
-        else:
-            curr += 1
-            
-    return t * n / d
+    return sum(sieve) - 1        
     
 if __name__ == '__main__':
-    main()
+    print euler72(1e6)
