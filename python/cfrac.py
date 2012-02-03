418ab9a8c2d83ff6fd815a12f094239e2f17fa5e
Jordan Scales <scalesjordan@gmail.com>
2012-02-02 23:16:09 -0500

    re-made cfrac.py with the help of ProjectEuler no. 64

--- python/cfrac.py
+++ python/cfrac.py
@@ -1,34 +1,35 @@
-def expand(n):
-    frac = []
-    while True:
-        a = int(n)
-        frac.append(a)
+#!/usr/bin/python
+from sys import argv
 
-        n -= a
-        if n < 0.0001: break
-        n = 1.0/n
-
-        if n == int(n):
-            frac.append(int(n))
-            break
-    return frac
-
-def compress(frac):
-    n = frac[-1]
-    d = 1
-    for i in range(2, len(frac)+1):
-        # XOR swap
-        n = n ^ d
-        d = n ^ d
-        n = n ^ d
-        n += d * frac[-i]
-    return [n, d]
-
-##k = str(raw_input('Enter a fraction [a/b]: ')).split('/')
-##v = float(int(k[0])) / int(k[1])
-##
-##f = expand(v)
-##print 'Continued fraction: %s' % f
-##
-##g = compress(f)
-##print 'Compressed: %s/%s' % (g[0], g[1])
+def sqrt_cont_frac(n):
+    head = int(n**0.5)
+    if head == n**0.5:
+        return '[%s]' % str(head)
+    
+    num = head
+    denom = n - head**2
+    
+    a = [head]
+    states = []
+    
+    while 1:
+        pull = (head + num) / denom
+        num -= pull*denom
+        
+        for state in states:
+            if state == [pull, num, denom]:
+                return '[%s;(%s)]' % (str(head), ','.join(map(str, a[1:])))
+                
+        states.append([pull, num, denom])
+        a.append(pull)
+        
+        denom = (n - num**2) / denom
+        num *= -1
+        
+if __name__ == '__main__':
+    if len(argv) < 2:
+        n = int(raw_input('n = ?: '))
+    else:
+        n = int(argv[1])
+        
+    print 'sqrt(%s) => %s' % (n, sqrt_cont_frac(n))
