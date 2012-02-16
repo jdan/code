946ef9ccaa99f06cd24370226dc29cfa834adabb
Jordan Scales <scalesjordan@gmail.com>
2012-02-16 16:51:35 -0500

    added request handlers

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,11 +1,11 @@
 var http = require('http');
 var url  = require('url');
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
             
-        route(pathname);
+        route(handle, pathname);
         
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Hello World');
