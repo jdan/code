e447161b776779f7d05a8fc9c88ce275122694d4
Jordan Scales <scalesjordan@gmail.com>
2012-02-16 22:37:23 -0500

    implemented image uploading and renaming - finished tutorial

--- nodejs/requestHandlers.js
+++ nodejs/requestHandlers.js
@@ -1,8 +1,8 @@
-var exec = require("child_process").exec;
-var querystring = require("querystring");
-var fs = require("fs");
+var querystring = require("querystring"),
+    fs = require("fs"),
+    formidable = require("formidable");
 
-function start(response, postData) {
+function start(response) {
     console.log("Request handler 'start' was called.");
     
     var body = '<html>' +
@@ -23,15 +23,31 @@ function start(response, postData) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent the text: "+
-        querystring.parse(postData).text);
-    response.end();
+    
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        
+        /* Possible error on Windows systems:
+           tried to rename to an already existing file */
+        fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+            if (err) {
+                fs.unlink("/tmp/test.png");
+                fs.rename(files.upload.path, "/tmp/test.png");
+            }
+        });
+        
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image:<br />");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
-function show(response, postData) {
+function show(response) {
     console.log("Request handler 'show' was called.");
     fs.readFile("/tmp/test.png", "binary", function(error, file) {
         if (error) {
