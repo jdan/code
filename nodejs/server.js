26fab2c54d6c05f0fe7bd7b1f71bdb13998c173a
Jordan Scales <scalesjordan@gmail.com>
2012-02-16 22:25:46 -0500

    added fs npm and show method for displaying pngs

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,9 +3,20 @@ var url  = require('url');
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
-            
-        route(handle, pathname, response);
+        console.log("Request for " + pathname + " received.");
+        
+        request.setEncoding("utf8");
+        
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("Received POST data chunk '" + postDataChunk + "'.");
+        });
+         
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
     }
     
     http.createServer(onRequest).listen(8888);
