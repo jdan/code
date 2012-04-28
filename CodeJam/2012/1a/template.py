708368ff3b9218a2be75e935a9ec3346ca4e0a21
Jordan Scales <scalesjordan@gmail.com>
2012-04-27 20:44:30 -0400

    added 1a folder with template python script

---
+++ CodeJam/2012/1a/template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Jordan Scales - http://jordanscales.com - http://github.com/prezjordan
+# CodeJam 2012 Round 1A
+
+from sys import argv
+
+def main():
+    f = open(argv[1])
+    n = int(f.readline())
+    
+    for i in range(n):
+        # do stuff here
+        
+if __name__ == '__main__':
+    main()
