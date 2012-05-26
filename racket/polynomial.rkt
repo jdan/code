ee1c72bc37ea0f996465f69afc7624b0978b04be
Jordan Scales <scalesjordan@gmail.com>
2012-05-25 22:54:48 -0400

    polynomial operations

---
+++ racket/polynomial.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+; polynomial functions
+; NOTE: polynomials are represented inversely
+; i.e. '(1 2 3) => 1 + 2x + 3x^2
+
+(define eval
+  (lambda (poly x)
+    (cond 
+      ((= (length poly) 0) 0)
+      ((= (length poly) 1) (car poly))
+      (else 
+        (+ (car poly) (* x (eval (cdr poly) x)))))))
+
+(define zeros
+  (lambda (n)
+    (if (< n 1)
+        '()
+        (cons 0 (zeros (- n 1))))))
+
+(define up-degree
+  (lambda (poly n)
+    (append (zeros n) poly)))
+
+(define scalar
+  (lambda (poly d)
+    (if (null? poly)
+        '()
+        (cons (* d (car poly)) (scalar (cdr poly) d)))))
+
+(define add
+  (lambda (p1 p2)
+    (cond
+      ((null? p1) p2)
+      ((null? p2) p1)
+      (else
+       (cons (+ (car p1) (car p2))
+             (add (cdr p1) (cdr p2)))))))
+
+(define multiply
+  (lambda (p1 p2)
+    (if (null? p2)
+        '(0)
+        (add (scalar p1 (car p2))
+             (multiply (up-degree p1 1) (cdr p2))))))
+
+(define compose
+  (lambda (ls)
+    (if (null? ls)
+        '(1)
+        (multiply (list (* -1 (car ls)) 1)
+                  (compose (cdr ls))))))
+        
