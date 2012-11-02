5add88e8da3713782f12d6dd50946f2c0d31c3ef
Jordan Scales <scalesjordan@gmail.com>
2012-11-01 23:01:43 -0400

    ignoring koans and stripeCTF

---
+++ haskell/fib.hs
@@ -0,0 +1,15 @@
+module Fibonacci where
+
+fibTuple :: (Integer, Integer, Integer) -> (Integer, Integer, Integer)
+fibTuple (x, y, 0) = (x, y, 0)
+fibTuple (x, y, n) = fibTuple (y, x + y, n - 1)
+
+fibFirst :: (Integer, Integer, Integer) -> Integer
+fibFirst (x, _, _) = x
+
+fib :: Integer -> Integer
+fib n = (fibFirst . fibTuple) (0, 1, n)
+
+main :: IO ()
+main = do
+  print (fib 10)
