4f4f83004bf80b5112a46a95be431ebbf1d89c7f
Jordan Scales <scalesjordan@gmail.com>
2012-11-03 12:39:00 -0400

    better fault detection with Maybe

--- haskell/binarytree.hs
+++ haskell/binarytree.hs
@@ -29,10 +29,10 @@ arrayToBST (x:xs) = arrayToBST' (x:xs) EmptyTree
     arrayToBST' (x:xs) t = arrayToBST' xs (insert t x)
 
 -- search the tree (this doesn't do much...)
-find :: (Ord a) => BinaryTree a -> a -> a
-find EmptyTree e = undefined
+find :: (Ord a) => BinaryTree a -> a -> (Maybe a)
+find EmptyTree e = Nothing
 find (Node val left right) e
-  | e == val  = e
+  | e == val  = Just e
   | e < val   = find left e
   | otherwise = find right e
 
@@ -43,15 +43,16 @@ isLeaf (Node _ EmptyTree EmptyTree) = True
 isLeaf (Node _ _ _)                 = False
 
 -- find the max element of a tree
-findMax :: (Ord a) => BinaryTree a -> a
-findMax EmptyTree              = undefined
-findMax (Node val _ EmptyTree) = val
+findMax :: (Ord a) => BinaryTree a -> Maybe a
+findMax EmptyTree              = Nothing
+findMax (Node val _ EmptyTree) = Just val
 findMax (Node _ _ right)       = findMax right
 
 -- removes the max element and produces a new teee
 removeMax :: (Ord a) => BinaryTree a -> BinaryTree a
-removeMax EmptyTree                    = EmptyTree
-removeMax (Node _ EmptyTree EmptyTree) = EmptyTree
-removeMax (Node _ left EmptyTree)      = Node (findMax left) (removeMax left) EmptyTree
-removeMax (Node val left right)        = Node val left (removeMax right)
+removeMax EmptyTree               = EmptyTree
+removeMax (Node _ left EmptyTree) = case findMax left of
+                                      Just val -> Node val (removeMax left) EmptyTree
+                                      Nothing  -> EmptyTree
+removeMax (Node val left right)   = Node val left (removeMax right)
 
