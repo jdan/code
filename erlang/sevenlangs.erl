cfa8fc602c31c4a77e67120e0e8fc3328a4b0e49
Jordan Scales <scalesjordan@gmail.com>
2013-04-24 14:38:48 -0400

    day 1: error messages and counting

--- erlang/sevenlangs.erl
+++ erlang/sevenlangs.erl
@@ -1,6 +1,17 @@
 -module(sevenlangs).
--export([numwords/1]).
+-export([numWords/1, countToTen/0, errorOutput/1]).
 
-numwords([]) -> 1;                            % Base case, one word?
-numwords([32 | Rest]) -> 1 + numwords(Rest);  % 32 = SPACE
-numwords([_  | Rest]) -> numwords(Rest).
+numWords([]) -> 1;                            % Base case, one word?
+numWords([32 | Rest]) -> 1 + numWords(Rest);  % 32 = SPACE
+numWords([_  | Rest]) -> numWords(Rest).
+
+countToTenHelper(0) -> [];
+countToTenHelper(N) -> [N | countToTenHelper(N-1)].
+
+reverse([]) -> [];
+reverse([H|T]) -> reverse(T) ++ [H].
+
+countToTen() -> reverse(countToTenHelper(10)).
+
+errorOutput(success) -> "success";
+errorOutput({error, Message}) -> "error: " ++ Message.
